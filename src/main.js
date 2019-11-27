4b1d0ef7824eaa5c1252d4c1ffd589c260122987
Jeff Hägerman <jh@sdc-automation.se>
2019-11-27 13:36:56 +0100

    nytt init

--- src/main.js
+++ src/main.js
@@ -2,73 +2,46 @@ const { core } = require('@actions/core');
 const { WebClient } = require('@slack/web-api');
 const fs = require('fs');
 
-// Store file data chunks in this array
-let chunks = [];
-// We can use this variable to store the final data
-let fileBuffer;
+async function run() {
+/*
+    const SLACK_CHANNEL = core.getInput('SLACK_CHANNEL', { required: true });
+    const SLACK_TOKEN = core.getInput('SLACK_TOKEN', { required: true });
+    const SLACK_TITLE = core.getInput('SLACK_TITLE', { required: true });
+    const SLACK_COMMENT = core.getInput('SLACK_COMMENT', { required: true });
+    const SLACK_FILE = core.getInput('SLACK_FILE', { required: true });
+    const SLACK_FILENAME = core.getInput('SLACK_FILENAME', { required: true });
+*/
+    const SLACK_CHANNEL = "dslink-java-elvaco";
+    const SLACK_TOKEN = "xoxb-549700893668-843124358353-5Epz9tNcBaqjIv8vTfot8qtP";
+    const SLACK_TITLE = "Titel"
+    const SLACK_COMMENT = "Kommentar";
+    const SLACK_FILE = "tmp/dslink-java-v2-elvaco-virtual.zip";
+    const SLACK_FILENAME = "dslink-java-v2-elvaco-virtual-v1.2.zip";
 
-// Read file into stream.Readable
-let fileStream = fs.createReadStream('text.txt');
+    try {
+        const web = new WebClient(SLACK_TOKEN);
 
-// An error occurred with the stream
-fileStream.once('error', (err) => {
-    // Be sure to handle this properly!
-    console.error(err); 
-});
-
-// File is done being read
-fileStream.once('end', () => {
-    // create the final data Buffer from data chunks;
-    start();
-    // Of course, you can do anything else you need to here, like emit an event!
-});
-
-// Data is flushed from fileStream in chunks,
-// this callback will be executed for each chunk
-fileStream.on('data', (chunk) => {
-    chunks.push(chunk); // push data chunk to array
-
-    // We can perform actions on the partial data we have so far!
-});
-
-function start() {
-    // https://hooks.slack.com/services/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
-    const web = new WebClient("xoxp-549700893668-792506314819-842613690337-fa7e0255b4ef00ffce016e5eca2d4a80");
-
-    // The current date
-    const currentTime = new Date().toTimeString();
-
-    (async () => {
-    // Use the `auth.test` method to find information about the installing user
-    const res = await web.auth.test()
-
-    // Find your user id to know where to send messages to
-    const userId = res.user_id;
-    console.log("Userid: " + userId);
-
-    // Use the `chat.postMessage` method to send a message from this app
-    await web.files.upload({
-        channel: userId,
-        file: Buffer.concat(chunks),
-        initial_comment: "Test from app"
-    });
-    /*
-    await web.chat.postMessage({
-        channel: userId,
-        text: `The current time is ${currentTime}`,
-    });
-    */
-    console.log('File posted!');
-    })();
-
-    /*
-    async function run() {
-        try { 
-        const ms = core.getInput('milliseconds');
-        console.log(`Waiting ${ms} milliseconds ...`);
-        } finally {
-
+        // upload file to slack and post it
+        var meta = await web.files.upload({
+            channels: SLACK_CHANNEL,
+            file: fs.createReadStream(SLACK_FILE),
+            title: SLACK_TITLE,
+            filename: SLACK_FILENAME,
+            initial_comment: SLACK_COMMENT
+        });
+        if (meta.ok) {
+            console.log("File posted!");
+//            core.debug('File posted!');
+        } else {
+            console.error("Error could not post file to slack!");
+//            core.setFailed('Error could not post file too slack!');
         }
+    } catch(Err) {
+        console.error(Err);
+//        core.setFailed("Error ", Err);
     }
-    */
+
+//    console.log(meta);
 }
+
+run();
