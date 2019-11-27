abc83d701a71201e3ba1baf90b0fbef9d2a0f518
Jeff Hägerman <jh@sdc-automation.se>
2019-11-27 13:54:08 +0100

    init

--- src/main.js
+++ src/main.js
@@ -10,14 +10,6 @@ async function run() {
     const SLACK_COMMENT = core.getInput('SLACK_COMMENT', { required: true });
     const SLACK_FILE = core.getInput('SLACK_FILE', { required: true });
     const SLACK_FILENAME = core.getInput('SLACK_FILENAME', { required: true });
-/*
-    const SLACK_CHANNEL = "dslink-java-elvaco";
-    const SLACK_TOKEN = "xoxb-549700893668-843124358353-5Epz9tNcBaqjIv8vTfot8qtP";
-    const SLACK_TITLE = "Titel"
-    const SLACK_COMMENT = "Kommentar";
-    const SLACK_FILE = "tmp/dslink-java-v2-elvaco-virtual.zip";
-    const SLACK_FILENAME = "dslink-java-v2-elvaco-virtual-v1.2.zip";
-    */
 
     try {
         const web = new WebClient(SLACK_TOKEN);
@@ -31,16 +23,12 @@ async function run() {
             initial_comment: SLACK_COMMENT
         });
         if (meta.ok) {
-//            console.log("File posted!");
             core.debug('File posted!');
         } else {
-//            console.error("Error could not post file to slack!");
             core.setFailed('Error could not post file too slack!');
         }
     } catch(Err) {
-//        console.error(Err);
+
         core.setFailed("Error ", Err);
     }
-
-//    console.log(meta);
 }
